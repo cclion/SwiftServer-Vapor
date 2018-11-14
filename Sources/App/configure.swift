8c9a07d68f66e1d3815ec4f5962fbd61dd105218
cclion <crisps_13@163.com>
2018-11-14 15:39:32 +0800

    注册服务配置

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,6 +11,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try routes(router)
     services.register(router, as: Router.self)
 
+    /// RegisterServerConfig
+    let serverConfig = NIOServerConfig.default(hostname:"0.0.0.0", port: 8887)
+    services.register(serverConfig)
+    
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
@@ -18,8 +22,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .file(path: "/Users/job/Desktop/VaporSever/vapor"))
-
+//    let sqlite = try SQLiteDatabase(storage: .file(path: "/Users/job/Desktop/VaporSever/vapor"))
+    let sqlite = try SQLiteDatabase(storage: .memory)
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
@@ -31,8 +35,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: User.self, database: .sqlite)
     migrations.add(model: AccessToken.self, database: .sqlite)
     migrations.add(model: Article.self, database: .sqlite)
-
-    
     services.register(migrations)
 
 }
